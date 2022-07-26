90e3e6ca206f40e654753ff6b7a50bf6019a5cd0
GustavoGomesDias <treeelevengg@gmail.com>
2022-07-26 16:38:02 -0300

    test: add clothes DTO

---
+++ src/tests/api/dtos/clothes.spec.ts
@@ -0,0 +1,65 @@
+/* eslint-disable no-useless-catch */
+import ClothesDTO from '@dtos/ClotesDTO';
+import { BadRequestErr } from '@err/BadRequestError';
+
+describe('Clothes DTO', () => {
+  test('Should return bad request erro if name min size is less than 8', () => {
+    const makeStub = () => {
+      try {
+        return new ClothesDTO(1, 'a', 1, 1, ['https://images.com']);
+      } catch (err) {
+        throw err;
+      }
+    };
+
+    expect(makeStub).toThrow(BadRequestErr);
+  });
+
+  test('Should return bad request erro if id is less than 0', () => {
+    const makeStub = () => {
+      try {
+        return new ClothesDTO(-1, 'aaaaaaaa', 1, 1, ['https://images.com']);
+      } catch (err) {
+        throw err;
+      }
+    };
+
+    expect(makeStub).toThrow(BadRequestErr);
+  });
+
+  test('Should return bad request erro if value is less than 0', () => {
+    const makeStub = () => {
+      try {
+        return new ClothesDTO(1, 'aaaaaaaa', -1, 1, ['https://images.com']);
+      } catch (err) {
+        throw err;
+      }
+    };
+
+    expect(makeStub).toThrow(BadRequestErr);
+  });
+
+  test('Should return bad request erro if quantity is less than 0', () => {
+    const makeStub = () => {
+      try {
+        return new ClothesDTO(1, 'aaaaaaaa', 1, -1, ['https://images.com']);
+      } catch (err) {
+        throw err;
+      }
+    };
+
+    expect(makeStub).toThrow(BadRequestErr);
+  });
+
+  test('Should return bad request erro if images list is empty', () => {
+    const makeStub = () => {
+      try {
+        return new ClothesDTO(1, 'aaaaaaaa', 1, -1, []);
+      } catch (err) {
+        throw err;
+      }
+    };
+
+    expect(makeStub).toThrow(BadRequestErr);
+  });
+});
