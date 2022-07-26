758d52b29a5932981ac53d18000cf7cd41069bc5
GustavoGomesDias <treeelevengg@gmail.com>
2022-07-26 16:13:10 -0300

    fix: add cleare description

--- src/api/decorators/validations/Min.ts
+++ src/api/decorators/validations/Min.ts
@@ -1,13 +1,13 @@
 import { BadRequestErr } from '@err/BadRequestError';
 
 export const Min = (limit: number, property: string) => function (target: Object, propertyKey: string) {
-  let value : string;
+  let value: string;
   const getter = function () {
     return value;
   };
   const setter = function (newVal: string) {
     if (newVal.length < limit) {
-      throw new BadRequestErr(`${property} precisa ser maior que ${limit}.`);
+      throw new BadRequestErr(`O tamanho de ${property} precisa ser maior que ${limit}.`);
     } else {
       value = newVal;
     }
