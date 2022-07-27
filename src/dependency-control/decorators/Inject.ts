1cb7dade7bc07bde7d844e6b20224a440a6e95c6
GustavoGomesDias <treeelevengg@gmail.com>
2022-07-27 18:00:19 -0300

    doc: add dependency injection doc

--- src/dependency-control/decorators/Inject.ts
+++ src/dependency-control/decorators/Inject.ts
@@ -1,6 +1,25 @@
 import Container from '@DI/Container';
 import { DependencyError } from '@err/DependencyError';
 
+/**
+ *
+ * @param injections A list of injection ID's. You must pass dependencies that have already been signed in the Container
+ * @return A class that extends passed by parameter
+ *
+ *
+ * @example Inject an encrypter in the class
+ *
+ * ```typescript
+ * @Inject(['Example'])
+ * class Injection {
+ *  public example: Exemaple;
+ *  constructor(exemple?: Example) {
+ *    this.example = example
+ *  }
+ * }
+ * ```
+ */
+
 export const Inject = (injections: string[]) => function injectionTarget <T extends { new(...args: any[]): {} }>(constructor: T): T | void {
   return class extends constructor {
     constructor(...args: any[]) {
--- src/dependency-control/decorators/MakeDependencies.ts
+++ src/dependency-control/decorators/MakeDependencies.ts
@@ -1,5 +1,15 @@
 import { dependencyFactory } from '@DI/dependencyFactory';
-
+/**
+ *
+ * @description In this case, way necessary add a decorator for dependency factory. This decorator needs to be placed at the start of the application.
+ *
+ * @example
+ * For express
+ * ```ts
+ * @MakeDependencies()
+ * class App {
+ * ```
+ */
 export const MakeDependencies = () => function injectionTarget <T extends { new(...args: any[]): {} }>(constructor: T): T | void {
   dependencyFactory();
 };
