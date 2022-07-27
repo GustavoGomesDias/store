51ee513fd95b23104383b49db31de9df6a72d8f4
GustavoGomesDias <treeelevengg@gmail.com>
2022-07-27 16:24:14 -0300

    feat: add check if is field is empty

---
+++ src/api/decorators/validations/NotEmpty.ts
@@ -0,0 +1,25 @@
+import { BadRequestErr } from '@err/BadRequestError';
+import { validationField } from '@helpers/validations/index';
+
+/**
+ *
+ * @param property - Is value for print in message.
+ * @description - Check if value is empty
+ */
+export const NotEmpty = (property: string) => function (target: Object, propertyKey: string) {
+  let value: string;
+  const getter = function () {
+    return value;
+  };
+  const setter = function (newVal: string) {
+    if (validationField(newVal)) {
+      throw new BadRequestErr(`${property} é obrigatório.`);
+    } else {
+      value = newVal;
+    }
+  };
+  Object.defineProperty(target, propertyKey, {
+    get: getter,
+    set: setter,
+  });
+};
