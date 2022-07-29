52e121220d1b3c67211d8ca72ede60566f2e94d5
GustavoGomesDias <treeelevengg@gmail.com>
2022-07-29 17:39:32 -0300

    feat: add base 64 image validation

--- src/helpers/validations/index.ts
+++ src/helpers/validations/index.ts
@@ -36,3 +36,17 @@ export const isValidObject = (obj: Record<any, any>) => {
 };
 
 export const validateUrl = (value: string) => /^(?:(?:(?:https?):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
+
+export const validateBase64Image = (image: string) => {
+  const [type, base64Info] = image.split(',');
+
+  const acceptedTypes = ['png', 'jpg', 'jpeg'];
+
+  if (!acceptedTypes.some((value) => type.includes(value))) {
+    return false;
+  }
+
+  const validation = /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$/;
+
+  return validation.test(base64Info);
+};
