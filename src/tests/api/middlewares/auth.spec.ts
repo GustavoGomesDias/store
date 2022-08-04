60ffc1575cdf535d9ce881423808ca9af092bb23
GustavoGomesDias <treeelevengg@gmail.com>
2022-08-04 16:16:09 -0300

    test: ensure authenticated function call services with correct value

--- src/tests/api/middlewares/auth.spec.ts
+++ src/tests/api/middlewares/auth.spec.ts
@@ -10,60 +10,94 @@ import UserDAOImp from '@db/DAO/imp/user/UserDAOImp';
 describe('Middleware - Auth', () => {
   beforeAll(() => jest.clearAllMocks());
 
-  test('Should Auth Middleware contains webToken', () => {
-    factory();
+  describe('Validations', () => {
+    test('Should Auth Middleware contains webToken', () => {
+      factory();
 
-    const auth = new Auth();
+      const auth = new Auth();
 
-    expect(auth['authTokenService']).toEqual(makeWebTokenStub());
-  });
-
-  test('Should Auth Middleware contains UserDAO', () => {
-    factory();
-
-    const auth = new Auth();
-
-    expect(auth['userDAO']).toEqual(userMock);
-  });
-
-  test('Should throw UnauthorizedError if not exists header', async () => {
-    const auth = new Auth();
-
-    await expect(auth.authentitcated({})).rejects.toThrow(UnauthorizedError);
-  });
-
-  test('Should throw UnauthorizedError if not exists authorization', async () => {
-    const auth = new Auth();
-
-    await expect(auth.authentitcated({
-      headers: {},
-    })).rejects.toThrow(UnauthorizedError);
-  });
-
-  test('Should throw UnauthorizedError if not is Bearer token', async () => {
-    const auth = new Auth();
-
-    await expect(auth.authentitcated({
-      headers: {
-        authorization: 'ahdahdahsuh',
-      },
-    })).rejects.toThrow(UnauthorizedError);
-  });
-
-  test('Should throw UnauthorizedError if user is undefined', async () => {
-    factory();
-    const auth = new Auth();
-
-    jest.spyOn(userMock, 'findById').mockImplementationOnce(async (id) => {
-      const result = await Promise.resolve(undefined);
-
-      return result;
+      expect(auth['authTokenService']).toEqual(makeWebTokenStub());
     });
 
-    await expect(auth.authentitcated({
-      headers: {
-        authorization: 'Bearer ahdahdahsuh',
-      },
-    })).rejects.toThrow(UnauthorizedError);
+    test('Should Auth Middleware contains UserDAO', () => {
+      factory();
+
+      const auth = new Auth();
+
+      expect(auth['userDAO']).toEqual(userMock);
+    });
+
+    test('Should throw UnauthorizedError if not exists header', async () => {
+      const auth = new Auth();
+
+      await expect(auth.authentitcated({})).rejects.toThrow(UnauthorizedError);
+    });
+
+    test('Should throw UnauthorizedError if not exists authorization', async () => {
+      const auth = new Auth();
+
+      await expect(auth.authentitcated({
+        headers: {},
+      })).rejects.toThrow(UnauthorizedError);
+    });
+
+    test('Should throw UnauthorizedError if not is Bearer token', async () => {
+      const auth = new Auth();
+
+      await expect(auth.authentitcated({
+        headers: {
+          authorization: 'ahdahdahsuh',
+        },
+      })).rejects.toThrow(UnauthorizedError);
+    });
+
+    test('Should throw UnauthorizedError if user is undefined', async () => {
+      factory();
+      const auth = new Auth();
+
+      jest.spyOn(userMock, 'findById').mockImplementationOnce(async (id) => {
+        const result = await Promise.resolve(undefined);
+
+        return result;
+      });
+
+      await expect(auth.authentitcated({
+        headers: {
+          authorization: 'Bearer ahdahdahsuh',
+        },
+      })).rejects.toThrow(UnauthorizedError);
+    });
+  });
+
+  describe('Services tests', () => {
+    test('Should call Auth Token with correct token', async () => {
+      factory();
+      const auth = new Auth();
+
+      const spy = jest.spyOn(auth['authTokenService'], 'verify');
+
+      await auth.authentitcated({
+        headers: {
+          authorization: 'Bearer ahdahdahsuh',
+        },
+      });
+
+      expect(spy).toHaveBeenCalledWith('ahdahdahsuh');
+    });
+
+    test('Should call UserDAO with correct id', async () => {
+      factory();
+      const auth = new Auth();
+
+      const spy = jest.spyOn(userMock, 'findById');
+
+      await auth.authentitcated({
+        headers: {
+          authorization: 'Bearer ahdahdahsuh',
+        },
+      });
+
+      expect(spy).toHaveBeenCalledWith(1);
+    });
   });
 });
