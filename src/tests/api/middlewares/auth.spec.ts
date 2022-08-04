57789b47380852102b0d0c8c5b5be0b17d0bb574
GustavoGomesDias <treeelevengg@gmail.com>
2022-08-04 16:08:48 -0300

    test: add validation auths

--- src/tests/api/middlewares/auth.spec.ts
+++ src/tests/api/middlewares/auth.spec.ts
@@ -1,8 +1,11 @@
 /* eslint-disable dot-notation */
+import request from 'supertest';
 import Auth from '@middlewares/Auth';
 import userMock from '@mocks/DAO/userMock';
 import factory from '@mocks/DI/factory';
 import { makeWebTokenStub } from '@mocks/services/webToken';
+import { UnauthorizedError } from '@err/UnauthorizedError';
+import UserDAOImp from '@db/DAO/imp/user/UserDAOImp';
 
 describe('Middleware - Auth', () => {
   beforeAll(() => jest.clearAllMocks());
@@ -21,5 +24,46 @@ describe('Middleware - Auth', () => {
     const auth = new Auth();
 
     expect(auth['userDAO']).toEqual(userMock);
+  });
+
+  test('Should throw UnauthorizedError if not exists header', async () => {
+    const auth = new Auth();
+
+    await expect(auth.authentitcated({})).rejects.toThrow(UnauthorizedError);
+  });
+
+  test('Should throw UnauthorizedError if not exists authorization', async () => {
+    const auth = new Auth();
+
+    await expect(auth.authentitcated({
+      headers: {},
+    })).rejects.toThrow(UnauthorizedError);
+  });
+
+  test('Should throw UnauthorizedError if not is Bearer token', async () => {
+    const auth = new Auth();
+
+    await expect(auth.authentitcated({
+      headers: {
+        authorization: 'ahdahdahsuh',
+      },
+    })).rejects.toThrow(UnauthorizedError);
+  });
+
+  test('Should throw UnauthorizedError if user is undefined', async () => {
+    factory();
+    const auth = new Auth();
+
+    jest.spyOn(userMock, 'findById').mockImplementationOnce(async (id) => {
+      const result = await Promise.resolve(undefined);
+
+      return result;
+    });
+
+    await expect(auth.authentitcated({
+      headers: {
+        authorization: 'Bearer ahdahdahsuh',
+      },
+    })).rejects.toThrow(UnauthorizedError);
   });
 });
