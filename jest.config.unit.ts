49dce3e67f681cef1daf7a04a1ad4f896d846ec5
GustavoGomesDias <treeelevengg@gmail.com>
2022-09-10 20:30:16 -0300

    fix: remove get reporters for run specific test (integration or unitary)

--- jest.config.unit.ts
+++ jest.config.unit.ts
@@ -154,7 +154,7 @@ export default {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  reporters: ['default', '<rootDir>/dist/src/helpers/report/test-tracker.js'],
+  // reporters: ['default', '<rootDir>/dist/src/helpers/report/test-tracker.js'],
 
   // Automatically reset mock state before every test
   // resetMocks: false,
